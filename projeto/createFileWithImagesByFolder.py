dcf9cb8737261b0648cfdbd04f7dbb82cb7eb667
AdolfoMatias <petadolfo@gmail.com>
2021-06-28 17:50:31 -0300

    Etapa - FINAL (ADOLFO) rodando em 300 epoch com GPU e plotando results: Buracos e Sem Buracos

--- projeto/createFileWithImagesByFolder.py
+++ projeto/createFileWithImagesByFolder.py
@@ -5,19 +5,38 @@ from pathlib import Path
 
 rootPathTrain = "./dataset/train"
 rootPathVal = "./dataset/valid"
+rootPathTest = "./dataset/test"
 TRAIN_FILE = "./train.txt"
-VAl_FILE = "./valid.txt"
+VAl_FILE = "./val.txt"
+TEST_FILE = "./test.txt"
 
+def createFileWithImages(roothPath, file):
+    jpg_files = [f for f in glob2.glob(rootPathTrain + "/**/*.jpg", recursive=True)]
 
-jpg_files = [f for f in glob2.glob(rootPathTrain + "/**/*.jpg", recursive=True)]
+    with open (TRAIN_FILE, "w") as train_file:
+        train_file.write("\n".join(str(item) for item in jpg_files))
 
-with open (TRAIN_FILE, "w") as train_file:
-    train_file.write("\n".join(str(item) for item in jpg_files))
+def createFileWithImages(roothPath, file):
+    jpg_files = [f for f in glob2.glob(rootPathVal+ "/**/*.jpg", recursive=True)]
+    
+    with open (VAl_FILE, "w") as val_file:
+        val_file.write("\n".join(str(item) for item in jpg_files))
+
+def createFileWithImages(roothPath, file):
+    jpg_files = [f for f in glob2.glob(rootPathTest+ "/**/*.jpg", recursive=True)]
+    
+    with open (TEST_FILE, "w") as test_file:
+        val_file.write("\n".join(str(item) for item in jpg_files))
+
 
 
     """
     Crie o scrip para gerar traino e validação -> faça isso utilizando métodos e condicional para identidade da classe
     """
+if __name__ == '__main__':
+    createFileWithImages(rootPathTrain, TRAIN_FILE)
+    createFileWithImages(rootPathVal, VAl_FILE)
+    createFileWithImages(rootPathVal, TEST_FILE)
 
 
     
